d317f77818f6df216781cec3b0e9d9070a9ea999
Konstantinos Loizas <konsloiz@gmail.com>
2021-04-23 18:37:20 +0200

    Updated title

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,14 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+      
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+            Fib Calculator - Kubernetes Version!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
